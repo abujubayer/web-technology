45b466f2d96170664b7241e5b6bed60b6c2ec21a
abu jubayer <59165487+abujubayer@users.noreply.github.com>
2023-02-18 22:01:40 +0600

    Delete PHP directory

--- PHP/form1.php
+++
@@ -1,9 +0,0 @@
-<form action="form2.php" method="post">
-  <label>Name: <input type="text" name="name" required></label><br><br>
-  <label>Email: <input type="email" name="email" required></label><br><br>
-  <label>Gender:
-    <input type="radio" name="gender" value="male" required>Male
-    <input type="radio" name="gender" value="female" required>Female
-  </label><br><br>
-  <input type="submit" value="Submit">
-</form>
